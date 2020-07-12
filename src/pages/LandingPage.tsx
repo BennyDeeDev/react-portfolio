7fbb101bf890cb30d98e8c6de096ee05ac714d94
Benjamin Derksen <b-derksen@mail.de>
2020-07-12 12:00:00 +0200

    typewriter working

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+//@ts-ignore
+import Typewriter from "typewriter-effect";
 
 export default function LandingPage() {
-	return <div>LandingPage</div>;
+	return (
+		<div id="typewriter">
+			<Typewriter
+				onInit={(typewriter) => {
+					typewriter
+						.pasteString('<span style="color: #4af626;"> user@b-derksen.de:~$ </span>')
+						.pauseFor(2500)
+						.typeString(" ./gretU")
+						.pauseFor(10)
+						.deleteChars(2)
+						.typeString("etUser <br>")
+						.pasteString(
+							"Hey, ich bin Benjamin Derksen <br> Ich bin ein ambitionierter Full-Stack Entwickler."
+						)
+						.start();
+				}}
+			/>
+		</div>
+	);
 }
