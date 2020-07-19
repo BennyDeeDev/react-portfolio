2f30dd22b7fe4f31c0794ef437844343ff143681
Benjamin Derksen <b-derksen@mail.de>
2020-07-19 18:36:36 +0200

    mobile wip

---
+++ src/sections/MobileWip.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function MobileWip() {
+	return (
+		<div>
+			<h2 className="px-4 py-8">
+				An der mobilen Version wird derzeit gearbeitet, bitte besuche diese Webseite mit deinem Computer.
+			</h2>
+		</div>
+	);
+}
